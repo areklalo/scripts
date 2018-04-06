e8a0eb7a2a67f7eaa99df643c46486688f87fa17
Arkadiusz Lalo <alalo@soldevelo.com>
2018-04-06 08:59:45 +0200

    Added script using to remove all DHIS programs data

---
+++ dhis_report_deletevalues.sh
@@ -0,0 +1,9 @@
+-- delete all program values
+delete from trackedentitydatavalue;
+delete from programstageinstance;
+delete from programinstance;
+delete from trackedentityattributevalue;
+delete from trackedentityinstance;
+-- delete all data values
+delete from datavalue;
+
